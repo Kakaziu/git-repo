ffc148f0dcbfcd1c754f8bece049d2b05ddcbb90
Kauã <kaua.borba03@gmail.com>
2024-06-07 00:02:48 -0300

    create buttons area

--- src/pages/IssuesPage/index.js
+++ src/pages/IssuesPage/index.js
@@ -1,19 +1,26 @@
 import { useParams } from "react-router-dom"
-import { Container, Title } from "./styles"
+import { Button, Buttons, Container, SubTitle, Title } from "./styles"
 import { useEffect, useState } from "react"
 import { toast } from "react-toastify"
 
 function IssuesPage() {
   const { owner, repo } = useParams()
+  const [thisRepo, setThisRepo] = useState(null)
   const [issues, setIssues] = useState([])
 
   useEffect(() => {
     async function loadData() {
       try {
-        const response = await fetch(`https://api.github.com/repos/${owner}/${repo}/issues`)
-        const json = await response.json()
+        const responseIssues = await fetch(`https://api.github.com/repos/${owner}/${repo}/issues`)
+        const jsonIssues = await responseIssues.json()
 
-        if(response.ok) setIssues(json)
+        const responseRepo = await fetch(`https://api.github.com/repos/${owner}/${repo}`)
+        const jsonRepo = await responseRepo.json()
+
+        if(responseIssues.ok && responseRepo.ok) {
+          setIssues(jsonIssues)
+          setThisRepo(jsonRepo)
+        }
         else toast.error("Not found")
       } catch(e) {
         console.log(e)
@@ -25,8 +32,14 @@ function IssuesPage() {
 
   return(
     <Container>
-      <Title>{owner}</Title>
-      <button onClick={() => console.log(issues)}>oi</button>
+      <Title>{repo.toUpperCase()}</Title>
+      <SubTitle>{thisRepo.description}</SubTitle>
+
+      <Buttons>
+        <Button>Todas</Button>
+        <Button>Abertas</Button>
+        <Button>Fechadas</Button>
+      </Buttons>
     </Container>
   )
 }
--- src/pages/IssuesPage/styles.js
+++ src/pages/IssuesPage/styles.js
@@ -1,9 +1,43 @@
 import { styled } from 'styled-components'
 
 export const Container = styled.div`
-
+  margin: auto;
+  width: 560px;
+  background-color: white;
+  border-radius: 4px;
+  padding: 20px;
+  margin-top: 40px;
+  position: relative;
 `
 
 export const Title = styled.h1`
-  
+  text-align: center;
+  color: #0d0f38;
+`
+
+export const SubTitle = styled.span`
+  display: block;
+  text-align: center;
+  margin-top: 10px;
+`
+
+export const Buttons = styled.div`
+  margin-top: 20px;
+  display: flex;
+  justify-content: space-around;
+  width: 43%;
+`
+
+export const Button = styled.button`
+  padding: 5px 12px;
+  background-color: #e8e8e8;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: 0.2s;
+
+  &:hover {
+    background-color: #2e8cff;
+    color: white;
+  }
 `
