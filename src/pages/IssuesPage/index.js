9656fee0cff797bea6164b10599c71b3e52524ea
Kauã <kaua.borba03@gmail.com>
2024-06-06 23:32:58 -0300

    create: load data function

--- src/pages/IssuesPage/index.js
+++ src/pages/IssuesPage/index.js
@@ -1,12 +1,32 @@
 import { useParams } from "react-router-dom"
-import { Container } from "./styles"
+import { Container, Title } from "./styles"
+import { useEffect, useState } from "react"
+import { toast } from "react-toastify"
 
 function IssuesPage() {
   const { owner, repo } = useParams()
+  const [issues, setIssues] = useState([])
+
+  useEffect(() => {
+    async function loadData() {
+      try {
+        const response = await fetch(`https://api.github.com/repos/${owner}/${repo}/issues`)
+        const json = await response.json()
+
+        if(response.ok) setIssues(json)
+        else toast.error("Not found")
+      } catch(e) {
+        console.log(e)
+      }
+    }
+
+    loadData()
+  }, [owner, repo])
 
   return(
     <Container>
-
+      <Title>{owner}</Title>
+      <button onClick={() => console.log(issues)}>oi</button>
     </Container>
   )
 }
